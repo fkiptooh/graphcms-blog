3e82bbdfc88510bb878436d4fbac1073e33795eb
fkiptooh <fkiptooh@gmail.com>
2023-04-03 10:05:23 +0300

    Adjacent Post added

--- pages/api/comments.js
+++ pages/api/comments.js
@@ -1,5 +1,3 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
 import { GraphQLClient, gql } from 'graphql-request';
 
 const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
@@ -23,7 +21,12 @@ export default async function asynchandler(req, res) {
     }
   `;
 
-  const result = await graphQLClient.request(query, req.body);
+  const result = await graphQLClient.request(query, {
+    name: req.body.name,
+    email: req.body.email,
+    comment: req.body.comment,
+    slug: req.body.slug,
+  });
 
   return res.status(200).send(result);
 }
